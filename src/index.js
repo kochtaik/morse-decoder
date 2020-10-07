95a04acb41d1216b0fe7cf2a83152927855849fa
kochtaik <kochtaik@gmail.com>
2020-10-07 12:39:51 +0300

    resolve task

--- src/index.js
+++ src/index.js
@@ -38,7 +38,32 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+  const splitIntoChars = (code) => {
+  const splittedCode = [];
+  for (let i = 0; i < code.length; i += 10) {
+    const decade = code.slice(i, i + 10);
+    splittedCode.push(decade);
+  }
+  return splittedCode;
+};
+  
+  const encodeToMorse = (str) => {
+    let result = '';
+    for (let i = 0; i < str.length; i += 2) {
+      const unit = str.slice(i, i + 2);
+      if (unit === '10') result += '.';
+      else result += '-';
+    }
+    return result;
+  };
+    
+  const splitted = splitIntoChars(expr);
+  return splitted.reduce((acc, value) => {
+    if (value === '**********') return acc += ' ';
+    if (value.startsWith('0')) value = value.slice(value.indexOf('1'));
+    const toMorse = encodeToMorse(value);
+    return acc += MORSE_TABLE[toMorse];
+  }, '');
 }
 
 module.exports = {
